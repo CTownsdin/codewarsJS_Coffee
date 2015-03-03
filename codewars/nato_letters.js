9fc829924b590ef61efcaa53d61264a363ccb284
CTownsdin <Townsdin@gmail.com>
2015-03-03 10:04:52 -0800

    quick commit

---
+++ codewars/nato_letters.js
@@ -0,0 +1,27 @@
+'use strict';
+var nato = (function() {
+  var letters =  {
+    "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
+    "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
+    "G": "Golf",   "H": "Hotel",   "I": "India",
+    "J": "Juliett","K": "Kilo",    "L": "Lima",
+    "M": "Mike",   "N": "November","O": "Oscar",
+    "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
+    "S": "Sierra", "T": "Tango",   "U": "Uniform",
+    "V": "Victor", "W": "Whiskey", "X": "X-ray",
+    "Y": "Yankee", "Z": "Zulu"
+  };
+
+  return function(word) {
+
+    var str = '';
+
+    for (var i=0, len=word.length; i<len; i++) {
+      str += letters[word.charAt(i).toUpperCase()] + ' ';
+    }
+
+    return str.trim();
+  };
+})();
+
+module.exports = nato;
---
+++ codewars/test_nato_letters.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var nato = require('./nato_letters');
+
+console.log(nato('cat'));
