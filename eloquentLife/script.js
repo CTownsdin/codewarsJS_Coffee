e67f1b403e1cbab496727d51db42709ff0e79699
CTownsdin <Townsdin@gmail.com>
2015-06-03 14:09:13 -0700

    Adding start of eloquent life mini project

---
+++ eloquentLife/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var plan = [
+  "############################",
+  "#      #        ##    o    #",
+  "#                          #",
+  "#                          #",
+  "#         #######          #",
+  "#              #           #",
+  "#             ##           #",
+  "#            ###           #",
+  "#                          #",
+  "#                          #",
+  "#              o           #",
+  "#     ###                  #",
+  "#     ###        o         #",
+  "#                         ##",
+  "############################"
+];
+
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
+Vector.prototype.plus = function(other) {
+  return new Vector(this.x + other.x, this.y + other.y);
+};
+
+var grid = ["top left", "top middle", "top right", 
+            "bottom left", "bottom middle", "bottom right"];
+
+
+
+
